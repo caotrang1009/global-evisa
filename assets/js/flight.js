f8c52dd26688632a6803751045245dbd637a4270
Cao Thị Thùy Trang <81607881+caotrang1009@users.noreply.github.com>
2023-12-09 09:54:45 +0700

    update

---
+++ assets/js/flight.js
@@ -0,0 +1,198 @@
+(function($) {
+	
+	"use strict";
+	
+	$(document).ready(function() {
+		var minGap = 0;
+		var filterOptions = ['price', 'airlines', 'stops', 'depart-time', 'landing-time', 'duration'];
+
+		initLoadElement();
+
+		$(".dropdown-menu a.dropdown-toggle").on("click", function(e) {
+			if (!$(this).next().hasClass("show")) {
+				$(this).parents(".dropdown-menu").first().find(".show").removeClass("show");
+			}
+			var subMenu = $(this).next(".dropdown-menu");
+			subMenu.toggleClass("show");
+			$(this).parents("li.nav-item.dropdown.show").on("hidden.bs.dropdown", function(e) {
+				$(".dropdown-submenu .show").removeClass("show");
+			});
+			return false;
+		});
+		$(".selectpicker").selectpicker();
+
+		$(window).bind('load', function() {
+			var multiRange = $(".multi-range");
+			multiRange.each(function(index, item) {
+				var inputRange = $(item).find("input[type='range']");
+				handleRangeFirst($(inputRange).first());
+				handleRangeLast($(inputRange).last());
+			});
+		});
+
+		$(".price-range, .duration-range").on("input", function() {
+			$(this).css('--val', $(this).val());
+			$(this).parent(".range").find(".range__output").text($(this).val())
+		});
+
+		$(".depart-time-1, .landing-time-1").on("input", function() {
+			handleRangeFirst($(this));
+		});
+
+		$(".depart-time-2, .landing-time-2").on("input", function() {
+			handleRangeLast($(this));
+		});
+
+		function handleRangeFirst(input) {
+			var range2 = $(input).next("input");
+			if (parseInt($(range2).val()) - parseInt($(input).val()) <= minGap) {
+				$(input).val(parseInt($(range2).val())-minGap);
+			}			
+			$(input).closest(".multi-range").find(".time-output-1").text($(input).val());			
+			fillColor(input, range2);
+		}
+		
+		function handleRangeLast(input) {
+			var range1 = $(input).prev("input");
+			if (parseInt($(input).val()) - parseInt($(range1).val()) <= minGap) {
+				$(input).val(parseInt($(range1).val())+minGap);
+			}
+			$(input).closest(".multi-range").find(".time-output-2").text($(input).val());
+			fillColor(range1, input);
+		}
+
+		function fillColor(valFirst, valLast) {
+			var slideMax = parseInt($(valFirst).attr('max'));
+			var precent1 = (parseInt($(valFirst).val()) / slideMax) * 100;
+			var precent2 = (parseInt($(valLast).val()) / slideMax) * 100;
+			$(valFirst).closest(".multi-range").find(".slide-track").attr('style', `background: linear-gradient(to right, #F3BF8F ${precent1}%, #3B7ACC ${precent1}%, #3B7ACC ${precent2}%, #F3BF8F ${precent2}%)`);
+		}
+
+		$('#filterModal').on('shown.bs.modal', function () {
+			showFilter();
+		});
+		$('#sortModal').on('shown.bs.modal', function () {
+			showSort();
+		});
+
+		$('#filterModal').on('hidden.bs.modal', function () {
+			hideFilter();
+		});
+		$('#sortModal').on('hidden.bs.modal', function () {
+			hideSort();
+		});
+
+		function hideFilter() {
+			$.each(filterOptions, function (index, option) {
+				var content = $('#filterModal').find(`.${option} .content`).clone(true);
+				$('#filterModal').find(`.${option}`).html('');
+				$(`#${option}`).append(content);
+			});
+		}
+		function hideSort() {
+			var content = $('#sortModal').find('.sort-options').clone(true);
+			$('#sortModal').find('.modal-body').html('');
+			$(`#sort .dropdown-menu`).append(content);
+		}
+
+		function showFilter() {
+			$.each(filterOptions, function (index, option) {
+				var content = $(`#${option} .content`).clone(true);
+				$(`#${option}`).html('');
+				$('#filterModal').find(`.${option}`).append(content);
+			});
+		}
+		function showSort() {
+			var content = $(`#sort .sort-options`).clone(true);
+			$(`#sort .dropdown-menu`).html('');
+			$('#sortModal').find('.modal-body').append(content);
+		}
+
+		$(window).resize(function () {
+			if ($(window).width() < 576) {
+				showFilter();
+				showSort();
+			} else {
+				$('#filterModal .close').trigger("click");
+				$('#sortModal .close').trigger("click");
+				hideFilter();
+				hideSort();
+			}
+		});
+	});
+	
+})(jQuery);
+
+var telinput = document.querySelector(".telinput");
+if (telinput !== null) {
+	var iti = intlTelInput(telinput, {
+		placeholderNumberType: "MOBILE",
+		utilsScript: "/assets/js/telinput/js/utils.js"
+	});
+}
+
+function initLoadElement() {
+	$('.datepicker').each(function() {
+		$(this).daterangepicker({
+			singleDatePicker: true,
+			locale: {
+				format: 'DD/MM/YYYY'
+			},
+			autoApply: true
+		});
+	});
+
+	var swiper = $(".swiper");
+	if (swiper.length) {
+		swiper.each(function() {
+			var slider = $(this);
+			var sliderCentered = slider.data("center-mode") ? slider.data("center-mode") : true;
+			var sliderItems = slider.data("items") ? slider.data("items") : 3;
+			var sliderItemsXl = slider.data("items-xl") ? slider.data("items-xl") : Number(sliderItems); //option: number (items in 1200 to end )
+			var sliderItemsLg = slider.data("items-lg") ? slider.data("items-lg") : Number(sliderItemsXl); //option: number (items in 992 to 1199 )
+			var sliderItemsMd = slider.data("items-md") ? slider.data("items-md") : Number(sliderItemsLg); //option: number (items in 768 to 991 )
+			var sliderItemsSm = slider.data("items-sm") ? slider.data("items-sm") : Number(sliderItemsMd); //option: number (items in 576 to 767 )
+			var sliderItemsXs = slider.data("items-xs") ? slider.data("items-xs") : Number(sliderItemsSm); //option: number (items in start to 575 )
+			var sliderSpace = slider.data("space-between") ? slider.data("space-between") : 50;
+			var slideParent = slider.data("parent");
+			var slideRewind = slider.attr("data-rewind") ? false : true;
+			var slideDirection = slider.data("direction") ? slider.data("direction") : 'horizontal';
+			var arrowLeft = slider.data("prev") ? slider.data("prev") : '.swiper-button-prev';
+			var arrowRight = slider.data("next") ? slider.data("next") : '.swiper-button-next';
+			
+			var swiper = new Swiper(slideParent+" .swiper", {
+				direction: slideDirection,
+				initialSlide: Math.floor(sliderItems/2),
+				breakpoints: {
+					576: {
+						slidesPerView: sliderItemsXs,
+						spaceBetween: 10,
+					},
+					768: {
+						slidesPerView: sliderItemsSm,
+						spaceBetween: sliderSpace,
+					},
+					992: {
+						slidesPerView: sliderItemsMd,
+						spaceBetween: sliderSpace,
+					},
+					1200: {
+						slidesPerView: sliderItems,
+						spaceBetween: sliderSpace
+					}
+				},
+				freeMode: true, 
+				centeredSlides: sliderCentered,
+				loop: slideRewind,
+				navigation: {
+					nextEl: slideParent + " " +arrowRight,
+					prevEl: slideParent + " " +arrowLeft,
+				},
+				pagination: {
+				el: slideParent + " .swiper-pagination",
+				}
+			});
+		});
+	}
+}
+
---
+++ assets/js/flight/search.airports.js
@@ -0,0 +1,260 @@
+$(document).ready(function() {
+	var direction		= "round-trip";
+	var travelClass		= "ECONOMY";
+	var adults			= 1;
+	var children		= 0;
+	var infants			= 0;
+	var totalPax		= adults + children + infants;
+	var startDate		= "";
+	var endDate			= "";
+	var multiFlightItem	= $(".multi-city-item").first().prop('outerHTML');
+	var flights			= $("#multiCityQty").val();
+	
+	function addPax(type) {
+		if (type == 'adult') {
+			adults++;
+			totalPax = adults + children + infants;			
+			showSelectElements();
+		}
+		else if (type == 'children') {
+			children++;
+			totalPax = adults + children + infants;
+			showSelectElements();
+		}
+		else if (type == 'infant') {
+			infants = Math.min(++infants, adults);
+			totalPax = adults + children + infants;
+			showSelectElements();
+		}
+	}
+	
+	function removePax(type) {
+		if (type == 'adult') {
+			adults = adults > 1 ?  adults - 1 : adults;
+			infants = Math.min(infants, adults);
+			totalPax = adults + children + infants;
+			showSelectElements();
+		}
+		else if (type == 'children') {
+			children = children > 0 ? children - 1 : children;
+			totalPax = adults + children + infants;
+			showSelectElements();
+		} else if (type == 'infant') {
+			infants = Math.max(0, Math.min(--infants, adults));
+			totalPax = adults + children + infants;
+			showSelectElements();
+		}
+	}
+	
+	function showSelectElements(e) {
+		$(".dropdown-direction .direction-item").each(function() {
+			if (direction == $(this).attr("data-value")) {
+				$(this).addClass("selected");
+			} else {
+				$(this).removeClass("selected");
+			}
+		});
+		$("#direction").val(direction);
+		$(".direction-text").text($("."+direction).text());
+		
+		$("#adults").val(adults);
+		$(".adults-text").text(adults);
+		
+		$("#children").val(children);
+		$(".children-text").text(children);
+		
+		$("#infants").val(infants);
+		$(".infants-text").text(infants);
+
+		$(".add-more-flight").hide();
+		$(".multi-city-control").hide();
+		$(".remove-flight").hide();
+		
+		if (totalPax <= 9) {
+			$(".pax-text").text(totalPax);
+		}
+		
+		$(".dropdown-travel-class .travel-class-item").each(function() {
+			if (travelClass == $(this).attr("data-value")) {
+				$(this).addClass("selected");
+			} else {
+				$(this).removeClass("selected");
+			}
+		});
+		$("#travelClass").val(travelClass);
+		$(".travel-class-value").text($("."+travelClass).text());
+		$("#multiCityQty").val(1);
+		$('.multi-city-control input').prop('disabled', true);
+		$('.round-trip-part').removeClass('col-sm-4');
+		
+		setDate($("#departureDate"), new Date());
+		setDate($("#returnDate"), $("#departureDate").val());
+		var departureDateMultiCity = $("[id*='departureDateMultiCity']");
+		if (departureDateMultiCity.length) {
+			departureDateMultiCity.each(function(index, item) {
+				var previousDate = (index > 0) ? $("#departureDateMultiCity_"+(index+1)) : $("#departureDate");
+				setDate($(item), previousDate.val());
+			});
+		}
+
+		if (direction == "round-trip") {
+			$(".round-trip-control").show();
+			$(".round-trip-control-input").addClass("input-curve");
+		} else if (direction == "multi-city") {
+			$(".multi-city-control").show();
+			$(".add-more-flight").show();
+			$("#multiCityQty").val(flights);
+			$('.multi-city-control input').prop('disabled', false);
+			$(".round-trip-control").hide();
+			$(".round-trip-control-input").removeClass("input-curve");
+			$('.round-trip-part').addClass('col-sm-4');
+		} else {
+			$(".round-trip-control").hide();
+			$(".round-trip-control-input").removeClass("input-curve");
+		}
+	}
+	
+	function setDate(datePicker, minDate) {
+		if (datePicker.length) {
+			datePicker.daterangepicker({
+				singleDatePicker: true,
+				minDate: minDate,
+				startDate: datePicker.val(),
+				endDate: datePicker.val(),
+				locale: {
+					format: 'DD/MM/YYYY'
+				},
+				autoApply: true
+			});
+		}
+	}
+
+	function addOneDay(date = new Date(), number = 1) {
+		date.setDate(date.getDate() + number);
+		return date;
+	}
+	
+	function checkDate(dateMultiCity = null) {
+		var departureDate = $("#departureDate").val();
+		var returnDate = $("#returnDate").val();
+		if (Date.parse(returnDate) < Date.parse(departureDate)) {
+			$("#returnDate").data("daterangepicker").setStartDate(departureDate);
+			$("#returnDate").data("daterangepicker").setEndDate(departureDate);
+		}
+		if (dateMultiCity != null && dateMultiCity.is("[id*='departureDateMultiCity']")) {
+			var number = dateMultiCity.attr("id").match(/\d+/g);
+			number = (!number) ? 1 : parseInt(number);
+			var nextInputDate = $("#departureDateMultiCity_"+(parseInt(number)+1));
+			if (Date.parse(nextInputDate.val()) < Date.parse(dateMultiCity.val())) {
+				nextInputDate.data("daterangepicker").setStartDate(addOneDay(new Date(dateMultiCity.val())));
+				nextInputDate.data("daterangepicker").setEndDate(addOneDay(new Date(dateMultiCity.val())));
+			}
+		}
+	}
+	
+	function initFlightForm() {
+		direction	= $("#direction").val();
+		travelClass	= $("#travelClass").val();
+		adults		= parseInt($("#adults").val());
+		children	= parseInt($("#children").val());
+		infants		= parseInt($("#infants").val());
+		totalPax	= adults + children + infants;
+		startDate	= $("#departureDate").val();
+		endDate		= $("#returnDate").val();
+		
+		showSelectElements();
+	}
+	
+	function updateMultiCityItem() {
+		$(".multi-city-item").each(function(index, item) {
+			$(this).find("[id*='originLocationCodeMultiCity_']").attr("id", "originLocationCodeMultiCity_" + (index+2));
+			$(this).find("[id*='destinationLocationCodeMultiCity']").attr("id", "destinationLocationCodeMultiCity_" + (index+2));
+			$(this).find("[id*='departureDateMultiCity']").attr("id", "departureDateMultiCity_" + (index+2));
+			$(this).find(".btn-exchange").attr("data-value", (index+1));
+			if (index != 0) {
+				$(this).find(".remove-flight").show();
+			}
+		});
+		$("#multiCityQty").val(flights);
+		
+		if (flights >= 6) {
+			$(".btn-add-flight").hide();
+		} else {
+			$(".btn-add-flight").show();
+		}
+	}
+	
+	$(".direction-item").click(function() {
+		direction = $(this).attr("data-value");
+		showSelectElements();
+	});
+
+	$(".pax-add").click(function() {
+		if (totalPax < 9) {
+			addPax($(this).attr("data-value"));
+		}
+	});
+
+	$(".pax-remove").click(function() {
+		removePax($(this).attr("data-value"));
+	});
+
+	$(".travel-class-item").click(function() {
+		travelClass = $(this).attr("data-value");
+		showSelectElements();
+	});
+	
+	$(document).on("click", ".btn-exchange", function() {
+		dataValue = $(this).data("value");
+		var originLocationCode = $("[id*='originLocationCode']").eq(dataValue).val();
+		$("[id*='originLocationCode']").eq(dataValue).val($("[id*='destinationLocationCode']").eq(dataValue).val());
+		$("[id*='destinationLocationCode']").eq(dataValue).val(originLocationCode);
+	});
+
+	$(".btn-add-flight").click(function() {
+		flights++;
+		$(".multi-city-control").append(multiFlightItem);
+		updateMultiCityItem();
+
+		setDate($("#departureDateMultiCity_"+flights), addOneDay(new Date($("#departureDateMultiCity_"+(flights-1)).val())));
+		$("#originLocationCodeMultiCity_"+flights).val($("#destinationLocationCodeMultiCity_"+(flights-1)).val());
+		$("#destinationLocationCodeMultiCity_"+flights).val("");
+	});
+
+	$(".multi-city-control").on("click", ".btn-remove", function() {
+		flights--;
+		$(this).closest('.multi-city-item').remove();
+		updateMultiCityItem();
+	});
+	
+	$(document).on("change", ".datepicker", function(){
+		checkDate($(this));
+		showSelectElements();
+	});
+	
+	initFlightForm();
+	
+	$("#frm-search").validate({
+		rules: {
+			"originLocationCode": {
+				required: true
+			},
+			"destinationLocationCode": {
+				required: true
+			},
+			"date": {
+				required: true
+			}
+		},
+		highlight: function(input){
+			$(input).addClass('error');
+		},
+		errorPlacement: function(error, element){}
+	});
+	
+	$(".btn-search").click(function(){
+		$(".spinner").removeClass("d-none");
+		$('.spinner .center-div').loader('spinner').show();
+		$("#frm-search").submit();
+	});
+});
